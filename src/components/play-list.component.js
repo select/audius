2f06a7990b3a16081356e3847aa5aaa2ec9517c6
Fa!ko Krause <falko@webpgr.com>
2016-11-06 12:56:26 +0100

    added footer with import and export button to palyist, export already working

--- src/components/play-list.component.js
+++ src/components/play-list.component.js
@@ -21,6 +21,19 @@ Vue.component('play-list', {
 		this.unsubscribe();
 	},
 	methods: {
+		exportPlayList() {
+			const data = {
+				playList: this.mediaPlayer.playList,
+				entities: this.mediaPlayer.entities,
+			}
+			var element = document.createElement('a');
+		  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(data)));
+		  element.setAttribute('download', 'audius.data.json');
+		  element.style.display = 'none';
+		  document.body.appendChild(element);
+		  element.click();
+		  document.body.removeChild(element);
+		}
 	},
 	template: `
 <div class="play-list">
@@ -28,5 +41,11 @@ Vue.component('play-list', {
 	<ul class="media-list">
 		<video-item v-for="id in mediaPlayer.playList" :video="mediaPlayer.entities[id]"></video-item>
 	</ul>
+	<div class="play-list-footer">
+		<ul>
+			<li>Import</li>
+			<li v-on:click="exportPlayList">Export</li>
+		</ul>
+	</div>
 </div>`,
 });
