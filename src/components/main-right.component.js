1da10a2681c4cda2780d31f657785248c7ae3a0e
Fa!ko Krause <falko@webpgr.com>
2016-11-09 14:58:49 +0100

    refactored more simple, only show search results when available

--- src/components/main-right.component.js
+++ src/components/main-right.component.js
@@ -6,17 +6,14 @@ import './main-right.component.sass';
 Vue.component('main-right', {
 	data() {
 		return {
-			mediaPlayer: store.getState().mediaPlayer,
-			website: store.getState().website,
+			state: store.getState(),
 			store,
 			Actions,
-			tabs: ['queue', 'search', 'about'],
 		};
 	},
 	created() {
 		this.unsubscribe = store.subscribe(() => {
-			this.mediaPlayer = store.getState().mediaPlayer;
-			this.website = store.getState().website;
+			this.state = store.getState();
 		});
 	},
 	beforeDestroy() {
@@ -28,21 +25,27 @@ Vue.component('main-right', {
 <div class="main-right">
 	<ul class="main-right__tabs">
 		<li
-			v-for="tab in tabs"
-			v-on:click="store.dispatch(Actions.setMainRightTab(tab))"
-			v-bind:class="{ active: website.mainRightTab == tab }">{{tab}}</li>
+			v-on:click="store.dispatch(Actions.setMainRightTab('queue'))"
+			v-bind:class="{ active: state.website.mainRightTab == 'queue' }">Queue</li>
+		<li
+			v-if="state.youtube.results.length"
+			v-on:click="store.dispatch(Actions.setMainRightTab('search'))"
+			v-bind:class="{ active: state.website.mainRightTab == 'search' }">Search</li>
+		<li
+			v-on:click="store.dispatch(Actions.setMainRightTab('about'))"
+			v-bind:class="{ active: state.website.mainRightTab == 'about' }">About</li>
 	</ul>
-	<div class="main-right__content" v-if="website.mainRightTab">
-		<about-player v-if="website.mainRightTab == 'about'"></about-player>
-		<search-results v-if="website.mainRightTab == 'search'"></search-results>
-		<queue v-if="website.mainRightTab == 'queue'"></queue>
+	<div class="main-right__content" v-if="state.website.mainRightTab">
+		<about-player v-if="state.website.mainRightTab == 'about'"></about-player>
+		<search-results v-if="state.website.mainRightTab == 'search'"></search-results>
+		<queue v-if="state.website.mainRightTab == 'queue'"></queue>
 	</div>
 	<div
 		class="main-right__player"
-		v-bind:class="{ full: !website.mainRightTab }">
+		v-bind:class="{ full: !state.website.mainRightTab }">
 		<span
 			v-on:click="store.dispatch(Actions.setMainRightTab(''))"
-			v-if="website.mainRightTab"
+			v-if="state.website.mainRightTab"
 			class="main-right__player-full-btn wmp-icon-unfold_more"></span>
 		<youtube-player></youtube-player>
 	</div>
