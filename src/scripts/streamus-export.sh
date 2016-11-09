302f19e3503520318b6d6030e9409e6d095f19a5
Fa!ko Krause <falko@webpgr.com>
2016-11-09 13:25:22 +0100

    export streamus websql storage to json file that audius can parse

--- src/scripts/streamus-export.sh
+++ src/scripts/streamus-export.sh
@@ -1,4 +1,16 @@
-sqlite3 "$HOME/.config/google-chrome/Profile 1/Local Storage/chrome-extension_jbnkffmindojffecdhbbmekbmkkfpmjd_0.localstorage" 'select value from ItemTable where key like "StreamItems%"'> dump.json
-sed -i 's/{"song/,{"song/g' dump.json
-echo "[$(cat dump.json)]" > dump.json
-sed -i 's/\[,/[/g' dump.json
+playlist=`sqlite3 "$@" 'select value from ItemTable where key="StreamItems";'`
+playlist="$(echo $playlist | sed 's/,/","/g')"
+
+sqlite3 "$@" 'select key,value from ItemTable where key like "StreamItems-%"'> streamus.dump.json
+sed -i 's/StreamItems/,"StreamItems/g' streamus.dump.json
+sed -i 's/|{"song/":{"song/g' streamus.dump.json
+echo "{\"StreamusDump\": true,\"playlist\": [\"$playlist\"], \"items\": {$(cat streamus.dump.json)}}" > streamus.dump.json
+sed -i 's/{,/{/g' streamus.dump.json
+
+sed -i 's/\\[^"]/X/g' streamus.dump.json
+
+echo ""
+echo "------ writing streamus.dump.json"
+echo ""
+echo "Validate your output with http://zaa.ch/jsonlint/"
+echo "then import it at http://audius.rockdapus.org/"
