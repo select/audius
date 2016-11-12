06b10b54df81ff3bf25fcff40a8961c103caf1f8
Fa!ko Krause <falko@webpgr.com>
2016-11-12 14:08:15 +0100

    store init now individual for each configuration

--- src/store.js
+++ src/store.js
@@ -1,10 +1,26 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { messageRelayMiddleware } from './utils/messageRelayMiddleware';
+import { dbMiddleware } from './utils/dbMiddleware';
 import reducers from './reducers';
 
+
 let store;
-if (process && (process.env.NODE_ENV === 'production' || process.env.extension === true )) {
-	store = createStore(reducers);
+if(process && (process.env.extension === true)) {
+	if (process && (process.env.NODE_ENV === 'production') || document.querySelectorAll('#audius-website').length) {
+		console.log('extension store production')
+		store = createStore(reducers, applyMiddleware(messageRelayMiddleware));
+	} else {
+		console.log('extension store dev')
+		const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+		store = createStore(reducers, composeEnhancers( applyMiddleware(...[messageRelayMiddleware]) ));
+	}
 } else {
-	store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+	console.log('website store production: ', process.env.NODE_ENV)
+	if (process && (process.env.NODE_ENV === 'production')) {
+		store = createStore(reducers, applyMiddleware(dbMiddleware));
+	} else {
+		const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+		store = createStore(reducers, composeEnhancers( applyMiddleware(...[dbMiddleware]) ));
+	}
 }
 export default store;
