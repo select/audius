2cdba1b8b699ca499a1df2fecb71a50d9dd8bdad
Fa!ko Krause <falko@webpgr.com>
2016-11-16 02:56:31 +0100

    code style and added save to indexdb after upgrade

--- src/utils/dbMiddleware.js
+++ src/utils/dbMiddleware.js
@@ -1,4 +1,5 @@
 import Actions from '../actions';
+import { time2s } from './timeConverter';
 
 function exists(store, storeName, id, callback) {
 	const request = store.getState().mediaPlayer.db
@@ -15,34 +16,46 @@ export function setPlayList(store) {
 			.transaction(['playLists'], 'readwrite')
 			.objectStore('playLists');
 		const action = event.target.result ? 'put' : 'add';
-		const request = dbStore[action]({ id: 'default', playList: store.getState().mediaPlayer.playList });
+		const request = dbStore[action]({
+			id: 'default',
+			playList: store.getState().mediaPlayer.playList,
+		});
 		request.onerror = event2 => store.dispatch(Actions.error(`DB Error ${event2.target.error.name}`));
 	});
 }
 
 export function setMediaEntity(store, data, actionType) {
-	console.log('setMediaEntity middleware!')
 	exists(store, 'mediaEntities', 'default', (event) => {
 		const dbStore = store.getState().mediaPlayer.db
 			.transaction(['mediaEntities'], 'readwrite')
 			.objectStore('mediaEntities');
 		const action = event.target.result ? 'put' : 'add';
 		const request = dbStore[action](data);
-		request.onsuccess = () => store.dispatch({type: `${actionType}_SUCCESS`});
+		// request.onsuccess = () => store.dispatch({type: `${actionType}_SUCCESS`});
 		request.onerror = event2 => store.dispatch(Actions.error(`DB Error ${event2.target.error.name}`));
 		setPlayList(store);
 	});
 }
 
-
 export const dbMiddleware = store => next => action => {
-	let result = next(action)
-  if (action.type === 'MOVE_PLAYLIST_MEDIA') {
-  	setPlayList(store);
+	const result = next(action);
+	if (action.type === 'MOVE_PLAYLIST_MEDIA') {
+		setPlayList(store);
 	}
 	if (['ADD_SEARCH_RESULT', 'REMOVE_VIDEO', 'VIDEO_ERROR'].includes(action.type)) {
-		console.log('middleware!!')
 		setMediaEntity(store, action.video, action.type);
 	}
-  return result
-}
+	return result;
+};
+
+export const upgradePlayListMiddleware = store => next => action => {
+	const result = next(action);
+	if (action.type === 'UPGRADE_PLAYLIST') {
+		const entities = store.getState().mediaPlayer.entities;
+		Object.keys(entities).forEach((key) => {
+			if (entities[key].durationS === 0) entities[key].durationS = time2s(entities[key].duration);
+			setMediaEntity(store, entities[key], action.type);
+		});
+	}
+	return result;
+};
