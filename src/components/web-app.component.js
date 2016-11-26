f2d0342fdd0dc8443180f33995a645644a3d5c93
Fa!ko Krause <falko@webpgr.com>
2016-11-26 18:08:30 +0100

    clean up

--- src/components/web-app.component.js
+++ src/components/web-app.component.js
@@ -4,22 +4,6 @@ import Actions from '../actions';
 import './web-app.component.sass';
 
 Vue.component('web-app', {
-	data() {
-		return {
-			website: store.getState().website,
-			store,
-			Actions,
-		};
-	},
-	created() {
-		this.unsubscribe = store.subscribe(() => {
-			console.log('change')
-			this.website = store.getState().website;
-		});
-	},
-	beforeDestroy() {
-		this.unsubscribe();
-	},
 	template: `
 <div class="web-app">
 	<web-header></web-header>
