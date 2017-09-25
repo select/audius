91118cecb90fea7966db54a2b77fd00efac25565
Fa!ko Krause <falko@webpgr.com>
2017-09-25 20:44:33 +0200

    use protocol agnostic url to prevent blocking

--- src/utils/vimeo.js
+++ src/utils/vimeo.js
@@ -5,7 +5,7 @@ import { s2time } from './timeConverter';
 export const isVimeoVideoRegEx = /https?:\/\/(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|video\/|)(\d+)(?:$|\/|\?)/;
 
 export function getVimeoInfo(id) {
-	return ajax(`http://vimeo.com/api/v2/video/${id}.json`).then(info => ([{
+	return ajax(`//vimeo.com/api/v2/video/${id}.json`).then(info => ([{
 		title: info[0].title,
 		duration: s2time(info[0].duration),
 		durationS: info[0].duration,
