409371f9a4baab9fe2f3add7547ecd1834ac612d
Fa!ko Krause <falko@webpgr.com>
2017-10-13 11:37:38 +0200

    sandbox did not get handshake, simplified, hopefully found error

--- src/extension/plugins-sandbox.js
+++ src/extension/plugins-sandbox.js
@@ -7,21 +7,29 @@ const pluginSandbox = {
 	init() {
 		window.addEventListener('message', event => {
 			logger('## sandbox event', event.data);
-			if (event.data.type === 'handshakeSandbox') {
+			if (!this.messageSource) {
+				logger('## sandbox handshake complete');
 				this.messageSource = event.source;
 				this.messageOrigin = event.origin;
-				// this.message({ type: 'handshakeSandbox' });
-			} else if (event.data.type === 'loadScript') this.loadScript(event.data);
+			}
+			if (event.data.type === 'loadScript') this.loadScript(event.data);
 			else if (event.data.type === 'getNext') this.getNext(event.data.id);
-			else if (event.data.type in this.parsers[event.data.id]) {
+			else if (event.data.id in this.parsers && event.data.type in this.parsers[event.data.id]) {
 				let parseResult;
 				try {
 					parseResult = this.parsers[event.data.id][event.data.type](event.data.data);
 				} catch (error) {
-					this.message({ audius: true, vuex: 'commit', type: 'error', data: `Error parsing ${event.data.id}. ${error}` });
+					this.message({
+						audius: true,
+						vuex: 'commit',
+						type: 'error',
+						data: `Error parsing ${event.data.id}. ${error}`,
+					});
 				}
 				if (['getYouTubeInfo', 'scanOneUrl', 'ajaxRaw', 'ajaxJSON'].includes(parseResult.type)) {
-					this.message(Object.assign(parseResult, { id: event.data.id, youtubeApiKey: this.youtubeApiKey }));
+					this.message(
+						Object.assign(parseResult, { id: event.data.id, youtubeApiKey: this.youtubeApiKey })
+					);
 				} else if (parseResult.type === 'mediaList') {
 					this.message({
 						audius: true,
@@ -35,7 +43,12 @@ const pluginSandbox = {
 				} else if (parseResult.type === 'noop') {
 					// Don't do anything, relax.
 				} else {
-					this.message({ audius: true, vuex: 'commit', type: 'error', data: `Unknown plugin command ${parseResult[0]}.` });
+					this.message({
+						audius: true,
+						vuex: 'commit',
+						type: 'error',
+						data: `Unknown plugin command ${parseResult[0]}.`,
+					});
 				}
 			}
 		});
@@ -49,19 +62,35 @@ const pluginSandbox = {
 			const result = this.parsers[id].getUrl();
 			this.message(Object.assign(result, { id })); // parser calls getJSON
 		} catch (error) {
-			this.message({ audius: true, vuex: 'commit', type: 'error', data: `Error getting next page. ${error}` });
+			this.message({
+				audius: true,
+				vuex: 'commit',
+				type: 'error',
+				data: `Error getting next page. ${error}`,
+			});
 		}
 	},
 	loadScript(options) {
 		const { id, code, youtubeApiKey } = options;
-		if (this.parsers[id]) this.message({ audius: true, vuex: 'commit', type: 'error', data: `Script '${id}' is already loaded` });
+		if (this.parsers[id])
+			this.message({
+				audius: true,
+				vuex: 'commit',
+				type: 'error',
+				data: `Script '${id}' is already loaded`,
+			});
 		try {
 			this.parsers[id] = eval(code);
 			if (options.responseTemplate) this.message(options.responseTemplate);
 			this.youtubeApiKey = youtubeApiKey;
 			this.getNext(id);
 		} catch (error) {
-			this.message({ audius: true, vuex: 'commit', type: 'error', data: `Error loading script. ${error}` });
+			this.message({
+				audius: true,
+				vuex: 'commit',
+				type: 'error',
+				data: `Error loading script. ${error}`,
+			});
 		}
 	},
 };
