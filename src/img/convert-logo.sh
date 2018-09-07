1df0005421633b5fa515b4eef72fb0bafd72d12e
Fa!ko Krause <falko@webpgr.com>
2018-09-07 20:03:44 +0200

    move to scripts

--- src/img/convert-logo.sh
+++
@@ -1,1 +0,0 @@
-for x in 16 48 96 128 192 257 512 1024; do inkscape --export-png audius.logo${x}.png -w ${x} audius.logo.square.svg ; done
---
+++ src/scripts/convert-logo.sh
@@ -0,0 +1,1 @@
+for x in 16 48 96 128 192 257 512 1024; do inkscape --export-png ../website/static/img/audius.logo${x}.png -w ${x} ../img/audius.logo.square.svg ; done
