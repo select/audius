82b13ac248def04c51fbe2659a2f9a7091cae919
Fa!ko Krause <falko@webpgr.com>
2018-09-09 23:25:06 +0200

    adapt to new webpack

--- config/webpack.production.extension.js
+++ config/webpack.production.extension.js
@@ -5,16 +5,12 @@ const webpack = require('webpack');
 const configDev = require('./webpack.dev.extension.js');
 
 module.exports = Object.assign(configDev, {
+	mode: 'production',
 	devtool: undefined,
 	plugins: [
 		...configDev.plugins,
 		new webpack.optimize.ModuleConcatenationPlugin(),
 		new webpack.optimize.OccurrenceOrderPlugin(),
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false,
-			},
-		}),
 		new webpack.DefinePlugin({
 			'process.env': {
 				NODE_ENV: JSON.stringify('production'),
