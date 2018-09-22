0e6936465d1308e51e5091c07ff9e691590e5545
Fa!ko Krause <falko@webpgr.com>
2018-09-22 19:06:18 +0200

    refactored code to DRY

--- src/extension/content.js
+++ src/extension/content.js
@@ -17,10 +17,34 @@ window.addEventListener(
 	},
 	false
 );
+
 let observer;
 const knownMedia = new Set();
+function scrapePage(request) {
+	const mediaList = webScraper
+		.findMediaInElement(document.body)
+		.filter(({ id }) => !knownMedia.has(id));
+	mediaList.forEach(({ id }) => knownMedia.add(id));
+	window.console.log('## Audius searching ', mediaList.length);
+	if (mediaList.length) {
+		chrome.runtime.sendMessage(
+			{
+				audius: true,
+				vuex: 'dispatch',
+				type: 'webScraperUpdateSuccess',
+				data: {
+					id: request.id,
+					mediaList,
+				},
+			},
+			() => {}
+		);
+	}
+	return mediaList;
+}
+
 // Listen to messages from background script and forward them to the audius website.
-chrome.runtime.onMessage.addListener((request, sender) => {
+chrome.runtime.onMessage.addListener(request => {
 	// if we are on a audius player page.
 	if (request.audius) {
 		if (isRunningInAudiusApp) {
@@ -36,44 +60,15 @@ chrome.runtime.onMessage.addListener((request, sender) => {
 				observer = new MutationObserver(
 					debounce(() => {
 						if (!watchChanges) return;
-						const mediaList = webScraper
-							.findMediaInElement(document.body)
-							.filter(({ id }) => !knownMedia.has(id));
-						mediaList.forEach(({ id }) => knownMedia.add(id));
-						// window.console.log('## Audius searching ', mediaList.length);
-						if (mediaList.length) {
-							chrome.runtime.sendMessage(
-								{
-									audius: true,
-									vuex: 'dispatch',
-									type: 'webScraperUpdateSuccess',
-									data: {
-										id: request.id,
-										mediaList,
-									},
-								},
-								() => {}
-							);
-						}
+						if (request.css && !document.querySelector(request.css)) return;
+						const mediaList = scrapePage(request);
+						window.console.log('## Audius searching ', mediaList.length);
 					}),
 					1000
 				);
-				const mediaList = webScraper.findMediaInElement(document.body).filter(({ id }) => !knownMedia.has(id));
-				mediaList.forEach(({ id }) => knownMedia.add(id));
-				window.console.log('## Audius inital scrape: ', mediaList.length);
-				if (mediaList.length) {
-					chrome.runtime.sendMessage(
-						{
-							audius: true,
-							vuex: 'dispatch',
-							type: 'webScraperUpdateSuccess',
-							data: {
-								id: request.id,
-								mediaList,
-							},
-						},
-						() => {}
-					);
+				if (!request.css || document.querySelector(request.css)) {
+					const mediaList = scrapePage(request);
+					window.console.log('## Audius inital scrape: ', mediaList.length);
 				}
 			}
 			// Start observing the DOM again
@@ -95,11 +90,7 @@ window.addEventListener(
 	'audiusExtension',
 	event => {
 		// window.console.log('content message relay request ', event.detail);
-		chrome.runtime.sendMessage(event.detail, response => {});
+		chrome.runtime.sendMessage(event.detail, () => {});
 	},
 	false
 );
-
-// Watch requested url
-// const url = new URL($el.value);
-// console.log("url.hostname", url.hostname);
